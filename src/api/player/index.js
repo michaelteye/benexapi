b3bcaaec7182f871e2ae9a77ca7874b7129eac2f
michaelteye <michaelteyegyemah@gmail.com>
2022-11-18 12:51:17 +0000

    take

--- src/api/player/index.js
+++ src/api/player/index.js
@@ -5,11 +5,16 @@
 
 import updatePlayerController from "./update.js"
 import createPlayerController from "./create.js"
-import deletePlayerController from "./delete.js"
+// import deletePlayerController from "./delete.js"
 import getList from "./getList.js";
 export default (app) => {
   app.put(`/player/:id`,updatePlayerController);
-  app.delete( `/player/:id`, deletePlayerController);
+  // app.delete( `/player/:id`, deletePlayerController);
   app.get('/player',getList)
   app.post(`/player`,createPlayerController);
+  app.delete(
+    `/player/:id`,
+    require('./delete').default
+  );
+
 };
